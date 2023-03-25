920cc030f9061c3f7c2a338da70d650cf6648ee0
MayaKroft <85786101+MayaKroft@users.noreply.github.com>
2023-03-25 12:44:07 +0100

    first template

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Necessary flask import
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 #create F app
 app = Flask(__name__)
@@ -7,8 +7,11 @@ app = Flask(__name__)
 #Assign function to be called upon the '/' path request
 @app.route("/")
 def index():
-    return 'Hello, world! This is the CD site'
+    text= {'h1' : 'Hello, world! This is the CD site' , 'p' : 'This is rendered through a template'}
+    return render_template('index.html', title = 'Index', data = text )
+    
 
 @app.route("/cow")
 def cow():
-    return 'MOoooOo!'
+    text= {'h1' : 'MOoooOo' , 'p' : 'MooOOoo'}
+    return render_template('index.html', title = 'Moo', data = text )
