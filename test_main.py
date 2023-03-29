6262739f14875a60060f459a56c0bf418bdf04b5
MayaKroft <85786101+MayaKroft@users.noreply.github.com>
2023-03-29 15:02:01 +0200

    test_doc_added

---
+++ test_main.py
@@ -0,0 +1,29 @@
+import pytest
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_redirect(client):
+    response = client.get("/home")
+    assert response.status_code == 302
+    assert response.location == "/"
+
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"<title>Index</title>" in response.data
+
+
+def test_cow(client):
+    response = client.get("/cow")
+    assert response.status_code == 200
+    assert b"<title>Moo</title>" in response.data
+
+
