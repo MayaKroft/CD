8664b0d953e26ec9455b91977b6c8bce9c7b2e23
Maya Kroft <mayakroft@hotmail.com>
2023-03-31 19:26:20 +0000

    yml change

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     text= {'h1' : 'Hello, world! This is the CD site!' , 'p' : 'This is rendered through a template'}
-    return render_template('main.html', title = 'Index', data = text )
+    return render_template('base.html', title = 'Index', data = text )
     
+@app.route("/home")
+def home():
+    return redirect(url_for("index"))
 
 @app.route("/cow")
 def cow():
     text= {'h1' : 'MOoooOo' , 'p' : 'MooOOoo'}
-    return render_template('main.html', title = 'Moo', data = text )
+    return render_template('base.html', title = 'Moo', data = text )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
